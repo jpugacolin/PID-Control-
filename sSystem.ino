520cc855561fc0ce740308a6414ff97f621947b2
Jose Puga <128130137+jpugacolin@users.noreply.github.com>
2023-03-18 02:20:39 -0700

    Create sSystem.ino

---
+++ sSystem.ino
@@ -0,0 +1,21 @@
+sBalancing Robot: System Program
+
+#include <Arduino.h>
+
+//MPU Control/Status Vars
+
+//Motor Controller 
+
+//Timers
+
+// IR Sensor & Distance Sensor 
+
+// Display Module 
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
